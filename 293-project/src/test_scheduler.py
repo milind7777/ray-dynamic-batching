795c267a8136d69c4626fd354b8aa639e90c470b
milind7777 <pmilind1999@gmail.com>
2024-12-03 00:12:41 -0800

    test

--- 293-project/src/test_scheduler.py
+++ 293-project/src/test_scheduler.py
@@ -86,7 +86,7 @@ class WorkloadGenerator:
                     second_start_time = time.time()
                     for i in range(int(rate)):
                         input_tensor = torch.randn(3, 224, 224)
-                        self.scheduler.submit_request(model_name, str(model_name) + str(time.time()), input_tensor)
+                        self.scheduler.submit_request(model, str(model) + str(time.time()), input_tensor)
                     second_end_time = time.time()
 
                 time_left = time_left - (second_end_time - second_start_time)
